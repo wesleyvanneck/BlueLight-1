093994bbcb724b7b70b73e5869b7bef234059d81
Dylan K. Taylor <odigiman@gmail.com>
2018-06-18 12:23:19 +0100

    Remove some fully qualified function calls PhpStorm can't see these or understand how they are being called, which is very annoying for bug hunting. Additionally, we already have the CodeOptimizer for this.

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -2174,7 +2174,7 @@ class Server{
 		$this->logger->logException($e, $trace);
 
 		$lastError = [
-			"type" => \get_class($e),
+			"type" => get_class($e),
 			"message" => $errstr,
 			"fullFile" => $e->getFile(),
 			"file" => $errfile,
